fa951e648c59c8708edf159ec07198cf3661e079
Dewi Aulia <dewaulia.nur@gmail.com>
2024-07-25 14:55:32 +0700

    Membuat list negara

--- app/src/main/java/com/dewiaulia/vsga2024/MainActivity.java
+++ app/src/main/java/com/dewiaulia/vsga2024/MainActivity.java
@@ -1,6 +1,9 @@
 package com.dewiaulia.vsga2024;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] nama = new String [] {
+            "Indonesia",
+            "Vietnam",
+            "Philipina",
+            "Thailand",
+            "Malaysia",
+            "Brunei Darussalam",
+            "Singapore",
+            "Timor Leste",
+            "Papua Nugini",
+            "Afghanistan",
+            "Arab Saudi",
+            "Dubai",
+            "Bangladesh",
+            "Myanmar",
+            "Kamboja",
+            "India",
+            "Srilanka",
+            "China"
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +48,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Arrays.sort(nama);
+
+        listView = findViewById(R.id.ListView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nama
+        );
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> Toast.makeText(
+                MainActivity.this,
+                nama[position] + " diklik",
+                Toast.LENGTH_LONG
+        ).show());
     }
 }
